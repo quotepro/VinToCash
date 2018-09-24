19770478bb4bf374495a3657b96ae662739c72f5
Brian <brian@quotepro.com>
2018-09-24 18:49:40 -0500

    fix for production api

--- src/app/vehicle-search/vehicle-search.component.ts
+++ src/app/vehicle-search/vehicle-search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DataService } from '@app/core/data.service';
 import { ChromaCar } from '@app/model/chroma-car';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vehicle-search',
@@ -12,7 +13,7 @@ export class VehicleSearchComponent implements OnInit {
 
   page: 0;
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService, private router: Router) { }
 
   ngOnInit() {
     this.data.getInventory();
@@ -26,6 +27,9 @@ export class VehicleSearchComponent implements OnInit {
     return this.data.vehicles;
   }
 
+  back() {
+    this.router.navigate(['buy']);
+  }
   search() {
     this.data.updateSession();
     this.data.getInventory();
